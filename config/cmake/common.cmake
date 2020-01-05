f144c66fa6c54e8644f0f70f4f5c4d116ab5399d
Rolf <rolf@rolf-mac-pro-2.home>
2020-01-05 23:29:28 +0100

    Initial commit

--- config/cmake/common.cmake
+++ config/cmake/common.cmake
@@ -4,50 +4,50 @@
 #
 ######################################################################
 
-set(COMMON_FLAGS_WARNINGS
-    "-Wall \
-    -Wextra \
-    -Wcast-align \
-    -Wchar-subscripts \
-    -Wcomment \
-    -Werror-implicit-function-declaration \
-    -Wfloat-equal \
-    -Wformat \
-    -Wformat=2 \
-    -Wlong-long \
-    -Wmain \
-    -Wno-type-limits \
-    -Wno-unused-parameter \
-    -Wno-expansion-to-defined \
-    -Wno-implicit-fallthrough \
-    -Wparentheses \
-    -Wpointer-arith \
-    -Wreturn-type \
-    -Wsequence-point \
-    -Wsign-compare \
-    -Wswitch \
-    -Wtrigraphs \
-    -Wundef \
-    -Wuninitialized \
-    -Wunknown-pragmas \
-    -Wunreachable-code \
-    -Wunused \
-    -Wwrite-strings"
+set(C_FLAGS_WARNINGS
+    "-Wall"
+    "-Wextra"
+    "-Wcast-align"
+    "-Wchar-subscripts"
+    "-Wcomment"
+    "-Werror-implicit-function-declaration"
+    "-Wfloat-equal"
+    "-Wformat"
+    "-Wformat=2"
+    "-Wlong-long"
+    "-Wmain"
+    "-Wno-type-limits"
+    "-Wno-unused-parameter"
+    "-Wno-expansion-to-defined"
+    "-Wno-implicit-fallthrough"
+    "-Wparentheses"
+    "-Wpointer-arith"
+    "-Wreturn-type"
+    "-Wsequence-point"
+    "-Wsign-compare"
+    "-Wswitch"
+    "-Wtrigraphs"
+    "-Wundef"
+    "-Wuninitialized"
+    "-Wunknown-pragmas"
+    "-Wunreachable-code"
+    "-Wunused"
+    "-Wwrite-strings"
 )
 
 set(C_FLAGS_WARNINGS_EXTRA
-    "-Waggregate-return \
-    -Wattributes \
-    -Wbad-function-cast \
-    -Wimplicit-int \
-    -Wmissing-declarations \
-    -Wmissing-format-attribute\
-    -Wmissing-prototypes \
-    -Wnested-externs \
-    -Wpacked \
-    -Wredundant-decls \
-    -Wshadow \
-    -Wstrict-prototypes"
+    "-Waggregate-return"
+    "-Wattributes"
+    "-Wbad-function-cast"
+    "-Wimplicit-int"
+    "-Wmissing-declarations"
+    "-Wmissing-format-attribute"
+    "-Wmissing-prototypes"
+    "-Wnested-externs"
+    "-Wpacked"
+    "-Wredundant-decls"
+    "-Wshadow"
+    "-Wstrict-prototypes"
 )
 
 ######################################################################
@@ -57,14 +57,14 @@ set(C_FLAGS_WARNINGS_EXTRA
 ######################################################################
 
 set(FLAGS_OPTIMIZATION
-    "-ffunction-sections \
-    -ffat-lto-objects \
-    -Os \
-    -fno-exceptions \
-    -fmessage-length=0 \
-    -fsigned-char \
-    -fdata-sections\
-    -g3"
+    "-ffunction-sections"
+    "-ffat-lto-objects"
+    "-Os"
+    "-fno-exceptions"
+    "-fmessage-length=0"
+    "-fsigned-char"
+    "-fdata-sections"
+    "-g3"
 )
 
 set(C_FLAGS "")
@@ -78,7 +78,7 @@ set(CXX_FLAGS "-std=gnu++11 -fdevirtualize-speculatively -fno-rtti")
 ######################################################################
 
 set(LINKER_FLAGS
-    "--specs=nosys.specs \
-    -L${LINKER_DIR} \
-    -Wl,--gc-sections"
+    "--specs=nosys.specs"
+    "-L${LINKER_DIR}"
+    "-Wl,--gc-sections"
 )
